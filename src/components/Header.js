e367234562666a1531b64d917d65a640d112031c
Saba Nouman <sabanuk03@gmail.com>
2019-10-16 12:12:53 +0100

    laerning components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <h1>My ToDo List</h1>
+        );
+
+
+    }
+
+}
+export default Header;
---
+++ src/components/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Intro extends React.Component {
+
+    render() {
+        return (
+            <p>Welcome to my new app</p>
+        );
+
+
+    }
+
+}
+export default Intro;
