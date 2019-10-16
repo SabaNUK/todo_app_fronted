66e54e3f1657bacc412033ce20ab012f6c855f21
Saba Nouman <sabanuk03@gmail.com>
2019-10-16 12:46:50 +0100

    Intro

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Intro from './components/Intro';
-
+import Task from './components/Task';
 function App() {
 
   return (
-    <div> 
+    <div className="container">
     <Header />
     <Intro />
+    <Task taskDescription="Buy some milk" />
+    <Task taskDescription="Pickup dog food" />
+    <Task taskDescription="Buy some chocolate" />
+    <Task taskDescription="Do homework" />
+
+
     </div>
   );
 }
